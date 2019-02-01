3c00f237f4ec400f8d669ce6fb64a85faffbee0b
Freepiehere <40921464+Freepiehere@users.noreply.github.com>
2019-01-31 22:50:01 -0500

    Add files via upload
    Need to redesign gametree

--- player.py
+++ player.py
@@ -1,4 +1,42 @@
-import gametree
+from gametree import GameTree
+
+# Player obejct interacts with gametree to navigate the gametree
 class Player():
-    def __init__(self):
-        pass
+    def __init__(self,team,gt=None):
+        if not gt:
+            print('make new gametree')
+            self.gt = GameTree(depth=0,max_depth=2)
+        else:
+            self.gt = gt
+        if not team:
+            self.team = gt.turn
+        else:
+            self.team = team
+        print('evaluate gametree')
+        self.gt.evaluate()
+        
+    def makeMove(self):
+        move_scores = []
+        child_index = 0
+        for child in self.gt.children:
+            move_scores.append(child.score)
+
+        if self.team:
+            child_index = move_scores.index(max(move_scores))
+        else:
+            child_index = move_scores.index(min(move_scores))
+        quit()
+        self.gt = GameTree(depth=0,max_depth=2,gt=self.gt.children[child])
+        self.gt.evaluate()
+
+if __name__ == "__main__":
+    print('begin')
+    gt = GameTree(depth=0,max_depth=2)
+    print('make player')
+    player = Player(gt.turn)
+    while not gt.board.is_checkmate():
+        print(player.gt.board)
+        player.makeMove()
+        gt = player.gt
+        player = Player(team=gt.turn,gt=gt)
+        
